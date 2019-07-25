15ee793d438767222a56099cc4c0161bc834378e
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-25 18:43:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,8 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
-    (params[:number1].to_i params[:operation] params[:number2].to_i).to_s
+    operation = params[:operation]
+    if operation = ''
   end
 
 end
