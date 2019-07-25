2440c8b8572336a2046a3f8cb80b60434a47a28d
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-25 18:47:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,8 +23,9 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     operation.downcase = params[:operation]
     num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
     if operation = 'add'
-      
+      "#{num1 + num2}"
   end
 
 end
